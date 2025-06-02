fe607fa78b98e7ea7cd18a765ffd23381823e09d
jolehuit <130084799+jolehuit@users.noreply.github.com>
2025-06-02 16:29:16 +0200

    Update Supabase authentication implementation

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,26 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next()
-  const supabase = createMiddlewareClient({ req: request, res: response })
 
-  await supabase.auth.getSession()
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get: (name) => request.cookies.get(name)?.value,
+        set: (name, value, options) => {
+          response.cookies.set(name, value, options)
+        },
+        remove: (name, options) => {
+          response.cookies.delete(name, options)
+        },
+      },
+    }
+  )
+
+  await supabase.auth.getUser()
 
   return response
 }
